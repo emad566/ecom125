72dfc0c4087b47b021c6979bcc66662c17cabc52
Emadeldeen Soliman <emade09@gmail.com>
2023-09-05 00:55:57 +0300

    frontend slider

--- app/Livewire/User/Home/HomeComponent.php
+++ app/Livewire/User/Home/HomeComponent.php
@@ -2,10 +2,17 @@
 
 namespace App\Livewire\User\Home;
 
+use App\Models\Slider;
 use Livewire\Component;
 
 class HomeComponent extends Component
 {
+    public $sliders;
+
+    public function mount()
+    {
+        $this->sliders = Slider::active()->get();
+    }
     public function render()
     {
         return view('livewire.user.home.home');
--- resources/views/livewire/user/home/sections/banner.blade.php
+++ resources/views/livewire/user/home/sections/banner.blade.php
@@ -7,39 +7,19 @@
             <div class="col-xl-12">
                 <div class="wsus__banner_content">
                     <div class="row banner_slider">
-                        <div class="col-xl-12">
-                            <div class="wsus__single_slider"
-                                 style="background: url({{asset('user/assets/images/slider_1.jpg')}});">
-                                <div class="wsus__single_slider_text">
-                                    <h3>new arrivals</h3>
-                                    <h1>men's fashion</h1>
-                                    <h6>start at $99.00</h6>
-                                    <a class="common_btn" href="#">shop now</a>
+                        @foreach($sliders as $slider)
+                            <div class="col-xl-12">
+                                <div class="wsus__single_slider"
+                                     style="background: url({{ $slider->banner_src }});">
+                                    <div class="wsus__single_slider_text">
+                                        <h3>{{$slider->title}}</h3>
+                                        <h1>{{$slider->type}}</h1>
+                                        <h6>{{__('start at')}} ${{$slider->starting_price}}</h6>
+                                        <a class="common_btn" href="{{$slider->btn_url}}">shop now</a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-xl-12">
-                            <div class="wsus__single_slider"
-                                 style="background: url({{asset('user/assets/images/slider_2.jpg')}});">
-                                <div class="wsus__single_slider_text">
-                                    <h3>new arrivals</h3>
-                                    <h1>kid's fashion</h1>
-                                    <h6>start at $49.00</h6>
-                                    <a class="common_btn" href="#">shop now</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-12">
-                            <div class="wsus__single_slider"
-                                 style="background: url({{asset('user/assets/images/slider_3.jpg')}});">
-                                <div class="wsus__single_slider_text">
-                                    <h3>new arrivals</h3>
-                                    <h1>winter collection</h1>
-                                    <h6>start at $99</h6>
-                                    <a class="common_btn" href="#">shop now</a>
-                                </div>
-                            </div>
-                        </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
